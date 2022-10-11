59c197156d84888d00d169962ed2717e17e3892c
rjames187 <rory.james2021@gmail.com>
2022-10-11 16:37:38 -0500

    complex state 1.6 unicafe step 1

---
+++ part1/complex-state-debugging/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+const Button = ({name, handler}) => {
+  return (
+    <button onClick={handler}>{name}</button>
+  )
+}
+
+const Display = ({good, neutral, bad}) => {
+  return (
+    <>
+      <div>Good: {good}</div>
+      <div>Neutral: {neutral}</div>
+      <div>Bad: {bad}</div>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const buttonHandler = (buttonName) => () => {
+  
+      switch (buttonName) {
+        case "good":
+          setGood(good + 1)
+          break;
+        case "neutral":
+          setNeutral(neutral + 1)
+          break;
+        case "bad":
+          setBad(bad + 1)
+          break;
+        default:
+          console.log("unknown button name")
+      }
+  }
+
+  return (
+    <>
+      <h1>Give Feedback</h1>
+      <Button name={"good"} handler={buttonHandler("good")}></Button>
+      <Button name={"neutral"} handler={buttonHandler("neutral")}></Button>
+      <Button name={"bad"} handler={buttonHandler("bad")}></Button>
+      <h1>Statistics</h1>
+      <Display good={good} neutral={neutral} bad={bad} />
+    </>
+  )
+}
+
+export default App
