5286693195f8cfc308ce8c16ecd7f4675834228b
rjames187 <rory.james2021@gmail.com>
2022-10-25 17:29:12 -0500

    2.8 phonebook step 3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
-  const [newName, setNewName] = useState('')
+  const [newName, setNewName] = useState('');
+  const [newNum, setNewNum] = useState('');
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
   }
 
+  const handleNumChange = (e) => {
+    setNewNum(e.target.value);
+  }
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    const nameObj = { name: newName };
+    const nameObj = { name: newName, number: newNum };
     if (persons.map(i => JSON.stringify(i)).includes(JSON.stringify(nameObj))) {
       alert(`${newName} is already added to phonebook`)
     } else {
@@ -26,13 +31,15 @@ const App = () => {
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input onChange={handleNameChange} />
+          <br/>
+          number: <input onChange={handleNumChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        { persons.map((i) => <div>{i.name}</div>) }
+        { persons.map((i) => <div>{i.name + ' ' + i.number}</div>) }
     </div>
   )
 }
