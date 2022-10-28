a8366c6ce9ec23012c335556033a2e321c6ca1bd
rjames187 <rory.james2021@gmail.com>
2022-10-28 00:00:39 -0500

    begin working on 2.12 data for countries step 1

--- part3/countries/src/App.js
+++ part3/countries/src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Countries = ({data, searchStr}) => {
+  const results = data.filter((i) => i.toLowerCase().includes(searchStr));
+
+  return (
+    <>
+      { 
+        results.length > 10 ? <p>Too many matches, specify another filter</p> : 
+        results.map((i, j) => <p key={j}>{i}</p>)
+      }
+    </>
+  )
+}
 
 function App() {
+
+  const [data, setData] = useState([]);
+  const [searchStr, setSearchStr] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+      const countries = res.data;
+      const names = countries.map((i) => i.name.common);
+      setData(names);
+    })
+  }, [])
+
+  const handleSearchChange = (e) => {
+    setSearchStr(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        find countries <input type="text" onChange={handleSearchChange}></input>
+      </div>
+      <Countries data={data} searchStr={searchStr}></Countries>
+    </>
   );
 }
 
