79f89e37853e4cbc30267cfe6bb698bed9f4bfb4
rjames187 <rory.james2021@gmail.com>
2022-12-08 20:12:44 -0600

    adding styles to react app lesson

--- part2/rendering-collection/src/App.js
+++ part2/rendering-collection/src/App.js
@@ -2,11 +2,14 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
+
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -21,9 +24,17 @@ const App = () => {
     const changedNote = { ...note, important: !note.important }
 
     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
+      .update(changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
       })
   }
 
@@ -54,6 +65,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
