626112ec8b10b56ae3333c3b61bfadd50e3b0ee9
rjames187 <rory.james2021@gmail.com>
2022-12-14 22:56:43 -0600

    3.1 phonebook backend step 1

--- part3real/phoneBackend/index.js
+++ part3real/phoneBackend/index.js
@@ -24,6 +24,12 @@ let persons = [
     }
 ]
 
+app.use(express.json())
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
