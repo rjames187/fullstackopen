30ed6181565965e275dced69f99ae9437f7dc220
rjames187 <rory.james2021@gmail.com>
2022-12-14 23:01:08 -0600

    3.2 phonebook backend step 2

--- part3real/phoneBackend/index.js
+++ part3real/phoneBackend/index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const numPersons = persons.length
+    const date = String(new Date())
+    response.send(`<p>Phonebook has info for ${numPersons} people</p><p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
