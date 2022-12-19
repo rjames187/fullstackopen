7885c931f32e03bcf748eb16f5fcc211c1f8dc68
rjames187 <rory.james2021@gmail.com>
2022-12-19 17:09:58 -0600

    4.5 helper functions and unit tests step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,13 @@ const totalLikes = (blogs) => {
   return blogs.map(mapper).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, cur) => cur.likes > prev.likes ? cur : prev
+  return blogs.reduce(reducer, blogs[0] ? blogs[0] : null)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
