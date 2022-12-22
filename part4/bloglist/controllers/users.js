86d6f5232d639a7419fdf2efe6f6265ff3e95580
rjames187 <rory.james2021@gmail.com>
2022-12-22 17:52:39 -0600

    4.15 bloglist expansion step 3

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,34 +3,33 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User
-      .find({}).populate('notes', { content: 1, date: 1 })
-  
+    const users = await User.find({}).populate('blogs')
+
     response.json(users)
-  });
+})
 
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
-  
+
     const existingUser = await User.findOne({ username })
     if (existingUser) {
-      return response.status(400).json({
-        error: 'username must be unique'
-      })
+        return response.status(400).json({
+            error: 'username must be unique'
+        })
     }
-  
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
-  
+
     const user = new User({
-      username,
-      name,
-      passwordHash,
+        username,
+        name,
+        passwordHash
     })
-  
+
     const savedUser = await user.save()
-  
+
     response.status(201).json(savedUser)
-  })
+})
 
 module.exports = usersRouter
