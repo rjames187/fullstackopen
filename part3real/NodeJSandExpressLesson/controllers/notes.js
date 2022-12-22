2e00140b9ac7e119775c2a3d19dcf94288a5dd1d
rjames187 <rory.james2021@gmail.com>
2022-12-22 17:21:21 -0600

    token authentication lesson

--- part3real/NodeJSandExpressLesson/controllers/notes.js
+++ part3real/NodeJSandExpressLesson/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note
@@ -17,10 +18,22 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
-  const body = request.body
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
-  const user = await User.findById(body.userId)
+notesRouter.post('/', async (request, response) => {
+  const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
@@ -32,7 +45,7 @@ notesRouter.post('/', async (request, response, next) => {
   const savedNote = await note.save()
   user.notes = user.notes.concat(savedNote._id)
   await user.save()
-  
+
   response.json(savedNote)
 })
 
