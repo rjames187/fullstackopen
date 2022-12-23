a5215e2609c5eb87b44b18e4843e0582242749c8
rjames187 <rory.james2021@gmail.com>
2022-12-22 18:27:47 -0600

    4.16 bloglist expansion step 4

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,33 +3,45 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('blogs')
+  const users = await User.find({}).populate('blogs')
 
-    response.json(users)
+  response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-    const existingUser = await User.findOne({ username })
-    if (existingUser) {
-        return response.status(400).json({
-            error: 'username must be unique'
-        })
-    }
-
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    const user = new User({
-        username,
-        name,
-        passwordHash
+  if (!username || !password) {
+    return response.status(400).json({
+      error: 'username and password are required'
     })
+  }
 
-    const savedUser = await user.save()
+  if (username.length < 3 || password.length < 3) {
+    return response.status(400).json({
+      error: 'username and password must be atleast 3 characters long'
+    })
+  }
 
-    response.status(201).json(savedUser)
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique'
+    })
+  }
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
