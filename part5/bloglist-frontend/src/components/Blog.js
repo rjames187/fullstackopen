7239932311c8a72c87065a8009fee74953d58385
rjames187 <rory.james2021@gmail.com>
2023-02-03 16:28:12 -0600

    5.7 bloglist frontend step 7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [detailsHidden, setDetailsHidden] = useState(true)
+
+  const buttonText = detailsHidden ? "view" : "hide"
+  const handleClick = () => setDetailsHidden(!detailsHidden)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title}</div>
+      <button onClick={handleClick}>{buttonText}</button>
+      <div style={{display: detailsHidden ? 'none' : ''}}>
+        <a href={blog.url}>{blog.url}</a>
+        <div style={{display: 'flex'}}>
+          <div>likes 0</div>
+          <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
