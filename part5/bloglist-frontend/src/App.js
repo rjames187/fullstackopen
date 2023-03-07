88a73a78eb1a2b24db74ee5d18b24894c89de85f
rjames187 <rory.james2021@gmail.com>
2023-03-06 22:27:49 -0600

    5.10 bloglist frontend step 10

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+      setBlogs(blogs.sort((a, b) => b.likes - a.likes))
     )  
   }, [])
 
